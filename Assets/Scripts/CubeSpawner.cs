49c7999239841e1cb60290108523fdd043dce91d
Edgars Skrabiņš <shiftkiro@gmail.com>
2024-06-22 14:12:03 +0300

    Cube spawner spawn location adjustment

--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -5,6 +5,8 @@ public class CubeSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject m_cubePrefab;
     [SerializeField] private Transform m_spawnerTF;
+    [SerializeField] private Transform m_playerTF;
+    [SerializeField] private Vector2 m_randomizedSpawnHeight;
     [SerializeField] private Vector2 m_spawnFrequency;
     [SerializeField] private Vector2 m_spawnAreaX;
     [SerializeField] private Vector2 m_spawnAreaZ;
@@ -14,6 +16,13 @@ public class CubeSpawner : MonoBehaviour
     private void Update()
     {
         CountSpawnTimer();
+        UpdateSpawnerPosition();
+    }
+
+    private void UpdateSpawnerPosition()
+    {
+        float spawnHeight = Random.Range(m_randomizedSpawnHeight.x, m_randomizedSpawnHeight.y);
+        m_spawnerTF.position = new Vector3(m_playerTF.position.x, m_playerTF.position.y - spawnHeight, m_playerTF.position.z);
     }
 
     private void CountSpawnTimer()
@@ -31,7 +40,7 @@ public class CubeSpawner : MonoBehaviour
     {
         float spawnPositionX = Random.Range(m_spawnAreaX.x, m_spawnAreaX.y);
         float spawnPositionZ = Random.Range(m_spawnAreaZ.x, m_spawnAreaZ.y);
-        Vector3 spawnPosition = new Vector3(spawnPositionX, m_spawnerTF.position.y, spawnPositionZ);
+        Vector3 spawnPosition = new Vector3(m_spawnerTF.position.x + spawnPositionX, m_spawnerTF.position.y, m_spawnerTF.position.z + spawnPositionZ);
         Instantiate(m_cubePrefab, spawnPosition, Quaternion.identity);
     }
 }
